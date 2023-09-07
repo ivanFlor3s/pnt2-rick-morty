eed7ed1a81464a3e01bcfb160d4ef3a5f3e1b6b2
ivanFlor3s <ifloresmanc96@gmail.com>
2023-09-06 21:17:15 -0300

    tabla en home

---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+    <header>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary m-0">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">RickAndMorty</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav gap-3 justify-content-end">
+                        <li class="nav-item">
+                            <RouterLink to="/">Home</RouterLink>
+                        </li>
+                        <li class="nav-item">
+                            <RouterLink to="/about">About</RouterLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import ApiTable from '@/components/ApiTable.vue';
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h2>Tabla de personajes</h2>
+    <hr>
+    <ApiTable />
   </main>
 </template>
