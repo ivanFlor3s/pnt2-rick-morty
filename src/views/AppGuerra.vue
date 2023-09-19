23e65a893c0a03fb5a1efe7dff0f398645a8df78
ivanFlor3s <ifloresmanc96@gmail.com>
2023-09-19 20:39:11 -0300

    turnos

--- src/views/AppGuerra.vue
+++ src/views/AppGuerra.vue
@@ -1,5 +1,10 @@
 <template>
     <div class="row">
+        <div class="col-12">
+            Turno de : {{ jugador1.esMiTurno ? jugador1.nombre : jugador2.nombre }}
+        </div>
+    </div>
+    <div class="row">
         <div class="col-6">
             <div class="card" >
                 <div class="card-body">
@@ -8,9 +13,9 @@
                     <img class="m-3" v-if="jugador1.stamina > 10000" src="https://media.tenor.com/qsvNX-PZDLkAAAAC/simpsons-jewels.gif" alt="homer">
 
                     <div class="d-flex gap-3">
-                        <button class="btn btn-danger" @click="lastimar(jugador2)">Lastimar</button>
-                        <button class="btn btn-success" @click="curar(jugador1, 10)">Curarse</button>
-                        <button class="btn btn-outline-primary" @click="curar(jugador1, 100000)">Vivir de angular</button>
+                        <button :disabled="!jugador1.esMiTurno" class="btn btn-danger" @click="lastimar(jugador2)">Lastimar</button>
+                        <button :disabled="!jugador1.esMiTurno" class="btn btn-success" @click="curar(jugador1, 10)">Curarse</button>
+                        <button :disabled="!jugador1.esMiTurno" class="btn btn-outline-primary" @click="curar(jugador1, 100000)">Vivir de angular</button>
                     </div>
                 </div>
             </div>
@@ -23,9 +28,9 @@
                     <img class="m-3" v-if="jugador2.stamina > 10000" src="https://media.tenor.com/qsvNX-PZDLkAAAAC/simpsons-jewels.gif" alt="homer">
 
                     <div class="d-flex gap-3">
-                        <button class="btn btn-success" @click="lastimar(jugador1)">Lastimar</button>
-                        <button class="btn btn-danger" @click="curar(jugador2, 10)">Curarse</button>
-                        <button class="btn btn-outline-primary" @click="curar(jugador2, 100000)">Vivir de angular</button>
+                        <button :disabled="!jugador2.esMiTurno"  class="btn btn-success" @click="lastimar(jugador1)">Lastimar</button>
+                        <button :disabled="!jugador2.esMiTurno" class="btn btn-danger" @click="curar(jugador2)">Curarse</button>
+                        <button :disabled="!jugador2.esMiTurno" class="btn btn-outline-primary" @click="curar(jugador2, 100000)">Vivir de angular</button>
                     </div>
                 </div>
             </div>
@@ -38,26 +43,39 @@ import { ref, onMounted } from 'vue'
 
 interface Jugador {
     stamina: number,
-    nombre: string
+    nombre: string,
+    esMiTurno: boolean
 }
 
 const lastimar = (jugador: Jugador) => {
-    jugador.stamina = jugador.stamina - 10
+    var numeroAleatorio = Math.floor(Math.random() * 15) + 5;
+    accionar(jugador, -numeroAleatorio)
 }
 
-const curar = (jugador: Jugador, cant: number) => {
-    jugador.stamina += cant;
+const curar = (jugador: Jugador, cant?: number) => {
+    var numeroAleatorio = Math.floor(Math.random() * 20) + 1;
+    accionar(jugador, cant || numeroAleatorio)
 }
-const jugador1 = ref({
+
+const accionar = (jugador: Jugador, valor: number) => {
+    jugador.stamina = jugador.stamina + valor
+    jugador1.value.esMiTurno = !jugador1.value.esMiTurno
+    jugador2.value.esMiTurno = !jugador2.value.esMiTurno
+}
+
+const jugador1 = ref<Jugador>({
     nombre: '',
-    stamina: 99
+    stamina: 99,
+    esMiTurno: false
 })
-const jugador2 = ref({
+const jugador2 = ref<Jugador>({
     nombre: '',
-    stamina: 100
+    stamina: 100,
+    esMiTurno: false
 })
 onMounted(() => {
     jugador1.value.nombre = 'Eduardo'
+    jugador1.value.esMiTurno = true 
     jugador2.value.nombre = 'Ricardo'
 });
 </script>
