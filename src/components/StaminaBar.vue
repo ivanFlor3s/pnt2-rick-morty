c7c3c5c529b24bcb16b34f742b14643f9b5f79af
ivanFlor3s <ifloresmanc96@gmail.com>
2023-09-21 20:04:19 -0300

    Stamina bar component

---
+++ src/components/StaminaBar.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="progress">
+        <div class="progress-bar" role="progressbar" aria-label="Example with label" 
+        :style="{ width: percent + '%' }">
+        </div>
+    </div> 
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+interface Props {
+    total: number
+    current: number
+}
+const props = defineProps<Props>()
+const percent = computed(() => {
+    return (props.current * 100) / props.total
+    })
+</script>
