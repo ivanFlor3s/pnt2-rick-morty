9715b959528649486bed4aa64d7f15f726029d5e
ivanFlor3s <ifloresmanc96@gmail.com>
2023-09-20 22:48:05 -0300

    correct form to moun app

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,4 @@ import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
 
-const app = createApp(App)
-const pinia = createPinia()
-
-app.use(router)
-app.use(pinia)
-app.mount('#app')
++ createApp(App).use(router).use(createPinia()).mount('#app');
