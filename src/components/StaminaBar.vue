b8f520297735abe0d16bb110fcb0a890ae55950f
ivanFlor3s <ifloresmanc96@gmail.com>
2023-09-21 20:10:17 -0300

    progress bar color by stamina

--- src/components/StaminaBar.vue
+++ src/components/StaminaBar.vue
@@ -1,20 +1,28 @@
 <template>
-    <div class="progress">
-        <div class="progress-bar" role="progressbar" aria-label="Example with label" 
-        :style="{ width: percent + '%' }">
-        </div>
-    </div> 
+  <div class="progress">
+    <div
+      class="progress-bar"
+      role="progressbar"
+      aria-label="Example with label"
+      :class="{
+        'bg-warning': percent > 40 && percent < 79,
+        'bg-danger': percent > 0 && percent < 39,
+        'bg-primary': percent > 80
+      }"
+      :style="{ width: percent + '%' }"
+    ></div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed } from 'vue'
 
 interface Props {
-    total: number
-    current: number
+  total: number
+  current: number
 }
 const props = defineProps<Props>()
 const percent = computed(() => {
-    return (props.current * 100) / props.total
-    })
+  return (props.current * 100) / props.total
+})
 </script>
