58e0eee915b1a19d75c4c397ad07f186c29bb4c1
ivanFlor3s <ifloresmanc96@gmail.com>
2023-09-20 23:43:07 -0300

    Button icons

--- src/components/GuerraPlayer.vue
+++ src/components/GuerraPlayer.vue
@@ -1,6 +1,5 @@
 <template>
     <div class="card">
-      
         <div class="card-body">
             <div v-if="player.hasTurn" class="end-0 m-2 position-absolute spinner-grow text-primary top-0 " role="status">
                 <span class="visually-hidden">Loading...</span>
@@ -15,12 +14,20 @@
                 <button class="btn btn-success">Curarse</button>
                 <button class="btn btn-outline-primary">Vivir de angular</button>
             </div>
+            <div class="d-flex gap-4">
+                <button class="btn btn-outline-danger">
+                    <i class="fa-solid fa-spoon fs-2"></i>
+                    <span>Cucharaso</span>
+                </button>
+                <button class="btn">
+                    <i class="fa-solid fa-house-medical text-success fs-2"></i>
+                </button>
+            </div>
         </div>
     </div>
 </template>
 <script setup lang="ts">
 import { type Player } from '../interfaces/player';
-
 interface Props {
     player: Player
     id: number
